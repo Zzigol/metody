b9ac15882564334a38ff65f8d4396bf5f89b1855
Zzigol <zigolenko@rambler.ru>
2022-04-22 17:06:25 +0300

    Второе решение

--- metodi2/Program.cs
+++ metodi2/Program.cs
@@ -4,17 +4,17 @@ namespace lesson
 {
     class Progpam
     {
-        static void Print(string symbol, uint symbolCount )
+        static void Print(char symbol, uint symbolCount)
         {
             for (int i = 0; i < symbolCount; i++) Console.Write(symbol);
         }
-        
-        static void Main(string[] args)
-       {
-            Console.WriteLine("Введите символ: ");
-            string symbol = Console.ReadLine();
 
-            Console.WriteLine("Введите число: ");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\nВведите число: ");
             uint symbolCount = uint.Parse(Console.ReadLine());
 
             Print(symbol, symbolCount);
