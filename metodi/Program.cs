94337679914db02e4e381bf211c3c9989e29999f
Zzigol <zigolenko@rambler.ru>
2022-04-22 16:38:37 +0300

    задача решена

--- metodi/Program.cs
+++ metodi/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace lesson
+{
+    class Progpam
+    {
+        
+
+
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Введите символ");
+            string simvol = Console.ReadLine();
+            Console.WriteLine("Введите количество символов");
+            int namber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < namber; i++) Console.Write(simvol);
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
